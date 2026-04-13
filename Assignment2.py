94cb71e3b36ed70c4c3888eccbe5898eabd23cad
Aitya Sharma <dev1973sharma@gmail.com>
2026-04-14 00:12:02 +0530

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -1,7 +1,7 @@
 
 """
-Name: Abhishek Kumar
-Roll No: 2301010322
+Name: Aditya Sharma
+Roll No: 2301010291
 Course: Image Processing & Computer Vision
 Unit: Mini Project
 Assignment Title: Noise Modeling and Image Restoration using Python
